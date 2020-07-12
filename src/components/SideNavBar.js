e5a6640381cabd7bd17f6f32a74e7d945a6e8c12
Matthew Chuong <mstchuong@yahoo.com>
2020-07-12 14:10:33 -0400

    Add icon for navbar

--- src/components/SideNavBar.js
+++ src/components/SideNavBar.js
@@ -1,21 +1,23 @@
 import React from "react";
 import { Link } from 'react-router-dom'
+import virus from '../img/virus.png'
 
 class SideNavBar extends React.Component {
   render() {
     return (
         <nav>
-           <h2>Matt's Corona Virus Stats</h2>
-           <div className="sidenavbar">
-           <ul>
-               <Link to="/" style={{textDecoration: 'none', color: "black"}}>
-                <li>Lookup by Country</li>
-               </Link>
-               <Link to="/Map" style={{textDecoration: 'none', color: "black"}}>
-                <li>Map</li>
-               </Link>
-           </ul>
-           </div>
+          <img alt="" src={virus} style={{maxWidth: '20%', float: 'left'}}/>
+          <h2>Coronavirus Stats</h2>
+          <div className="sidenavbar">
+          <ul>
+              <Link to="/" style={{textDecoration: 'none', color: "black"}}>
+              <li>Lookup by Country</li>
+              </Link>
+              <Link to="/Map" style={{textDecoration: 'none', color: "black"}}>
+              <li>Map</li>
+              </Link>
+          </ul>
+          </div>
         </nav>
     )
   }
