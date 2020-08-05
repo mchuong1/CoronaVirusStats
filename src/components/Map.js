d2b3f6683d9909340cb1c77fc518a80be8eb02d0
Matthew Chuong <mstchuong@yahoo.com>
2020-08-04 21:51:36 -0400

    Add GeoMap
    -add dependency react-google-charts

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,6 @@
-import React from "react";
-
-
+import React from "react"
+import Chart from 'react-google-charts'
+import googlemaps_apikey from '../resource/key.json'
 
 class Map extends React.Component{
     
@@ -8,7 +8,21 @@ class Map extends React.Component{
     render(){
         return(
             <div className="map">
-
+                <Chart 
+                width={'1200px'}
+                height={'600px'}
+                chartType="GeoChart"
+                data={[
+                    ['Country', 'Popularity'],
+                    ['Germany', 200],
+                    ['United States', 300],
+                    ['Brazil', 400],
+                    ['Canada', 500],
+                    ['France', 600],
+                    ['RU', 700],
+                  ]}
+                mapsApiKey={googlemaps_apikey}
+                />
             </div>
         )
     }
